3ad7754e8d36282f0ff1012f50b4205883600fa2
unknown <mariamheiba@gmail.com>
2019-03-22 14:19:45 +0200

    interceptor

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -9,17 +9,15 @@ export class AuthInterceptor implements HttpInterceptor{
 
 constructor(private router: Router){}
 intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>{
-    if (req.headers.has(InterceptorSkipHeader))
+    
+     if(localStorage.getItem('token')!=null)
     {
-        return next.handle(req.clone());
-    }
-    else if(localStorage.getItem('token')!=null)
-    {
-       
-        const clonedReq= req.clone({headers: req.headers.set('Authorization','Bearer'+localStorage.getItem('token'))});
+        
+        const clonedReq= req.clone({headers: req.headers.set('Authorization','bearer '+localStorage.getItem('token'))});
         return next.handle(clonedReq).pipe( tap(
             succ =>{},
             err=> {
+                
                 if(err.status ==401)
                 {localStorage.removeItem('token');
                 this.router.navigateByUrl('/login');}

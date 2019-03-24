b8e688b7280bbabcc9f04a36bd07fc30890fe295
sarah11 <sararaafat1@yahoo.com>
2019-03-24 02:24:45 +0200

    documentation  done

--- src/app/classes/book.ts
+++ src/app/classes/book.ts
@@ -7,11 +7,7 @@ export class book{
     /** all info about book */
   /** isbn is unique for each book */ 
     isbn: number;
-<<<<<<< HEAD
-    /** cover of book */
-=======
   /** book image*/
->>>>>>> 7ab2de2a26f792005aee4788da8fab3a21d3495e
     imageUrl: string;
   /** number of book pages*/
     pagesNum: number;
@@ -21,19 +17,11 @@ export class book{
     publisher: string;
   /** book langauge*/
     language: string;
-<<<<<<< HEAD
-    /** ratings of book */
-    ratingsAvg: number;
-    ratingsCount: number;
-    reviewsCount: number;
-    /** description about book */
-=======
   /** avrage rating of the book*/
     ratingsAvg: number;
     ratingsCount: number;
     reviewsCount: number;
   /** summry of the book*/
->>>>>>> 7ab2de2a26f792005aee4788da8fab3a21d3495e
     description: string;    
     authorId: number;
     /** author name of book */

19b8196bd07e9af400352644a72f76ba2f58a034
unknown <mariamheiba@gmail.com>
2019-03-25 15:53:12 +0200

    merge

--- src/app/fake-backend.ts
+++ src/app/fake-backend.ts
@@ -48,11 +48,11 @@ export class FakeBackendInterceptor implements HttpInterceptor {
             }
 
 
-            if (request.url.endsWith('/updates') && request.method === 'get') {
+            if (request.url.endsWith('/updates') && request.method === 'GET') {
                 // check for fake auth token in header and return users if valid, this security is implemented server side in a real application
-                console.log('enter');
+                
                 if (request.headers.get('Authorization') === 'bearer fake-jwt-token') {
-                    console.log('holaaa');
+                    
                     
                     return next.handle(request);
                 } else {

4868e518bdff948f594d71f2d8b2653429c1a2e1
SarahMohamedLotfy <sarahlotfy206@gmail.com>
2019-04-05 17:31:02 +0200

    MyBooks Page

---
+++ src/app/classes/myBooks.ts
@@ -0,0 +1,31 @@
+export class myBooks {
+    /**
+  * Id of the user following the main user .
+  */
+   id: number;
+   /**
+  * Name of the user following the main user .
+  */
+   name: string;
+   /**
+  * Image of the user following the main user .
+  */
+   image_url: string;
+   /**
+  * Id of the book tht the user is currently reading .
+  */
+   bookid:number;
+   /**
+  * Name of the book tht the user is currently reading .
+  */
+   bookname:string; 
+  /**
+  * Image of the book tht the user is currently reading .
+  */ 
+   bookimage:string;
+   /**
+  * Country of the user following the main user  .
+  */ 
+ country :string;
+ }
+ 
--- src/app/my-books/my-books.component.ts
+++ src/app/my-books/my-books.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { profile } from '../classes/profile';
+import { HttpService } from '../http.service';
+import {HttpmybooksService} from './httpmybooks.service';
+import {ProfileService} from '../profile/profile.service';
 @Component({
   selector: 'app-my-books',
   templateUrl: './my-books.component.html',
@@ -7,9 +13,81 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyBooksComponent implements OnInit {
 
-  constructor() { }
+ /**
+ * posts  is array of the data of people following  the main users ( id , name ,image,bookid,bookname,bookimage ) .
 
-  ngOnInit() {
-  }
+ */
+posts:any=[];
+/**
+* selectedProfile is the profile of the main user  who logged in . 
+*/
+ selectedProfile: profile ;
+  /**
+* temp is array of people following the main users .
+*/
+ temp: any =[];
+  /**
+* Search input text in search box .
+*/
+ searchText: string = ''
+/**
+*Constructer that take service and routing .
+*/
+ constructor(private myfirstservice :HttpmybooksService,private profileservice :ProfileService,private route: ActivatedRoute,private router:Router ) { }
+
+
+/**
+* Contains Get requests to get following list and books they are currently reading   and to get the profile data to get the name of the main user .
+* Getfollowing () is a get request ti get the data of people following the main user 
+* and the books they are currently reading . 
+* The data i get is ( name of user , image of user  , id of user  , bookid, bookname,bookimage).
+* GetUserprofile get the data of the profile of main user i used it to get the name of the main  user
+*/
+ ngOnInit() {
+   
+
+
+  this.myfirstservice.getfollowing().subscribe((posts:any)=>{
+     this.posts =posts ;
+     this.temp = posts;});
+
+
+     this.profileservice.getUserprofile(90).subscribe(
+       data => {
+         this.selectedProfile = data,
+         (err: any) => console.log(err),
+         console.log(this.selectedProfile)
+                }) ;
+ 
+ }
+
+/**
+*Search for the name of following person when click on search button  .
+*/ 
+search(){
+
+ if (!this.posts) {
+   return [];
+ }
+ if (this.searchText=='') {
+   this.posts = this.temp;
+ }
+ this.searchText = this.searchText.toLocaleLowerCase();
+/**
+*Filter for names of following people.
+*/ 
+ this.posts = this.temp.filter(it => 
+   it["name"].toLocaleLowerCase().includes(this.searchText)
+ );
+}
+//onclick(){
+// this.router.navigateByUrl('/bookinfo/https://my-json-server.typicode.com/SarahMohamedAhmed/followinggg/following/id');
+//}
+
+
+
+
+
+
 
 }

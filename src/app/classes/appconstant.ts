43b86e7a3805a182508fd8801ca3608b09571890
unknown <mariamheiba@gmail.com>
2019-05-04 19:10:53 +0200

    url

--- src/app/classes/appconstant.ts
+++ src/app/classes/appconstant.ts
@@ -1,5 +1,5 @@
 export class AppConstants {
     public static get baseURL(): string { 
-    //return "http://ec2-52-90-5-77.compute-1.amazonaws.com" ;}
-   return "http://f346d84b.ngrok.io" ;}
+    return "http://ec2-52-90-5-77.compute-1.amazonaws.com" ;}
+   //return "http://f346d84b.ngrok.io" ;}
 }

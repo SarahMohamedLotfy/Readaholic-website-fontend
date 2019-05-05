bbe4bb391132d9adbfaac417d348f48798c55407
TMai-mostafa99 <maimostafaibrahim@gmail.com>
2019-05-05 04:04:01 +0200

    sync comments and likes

--- src/app/shared/list-likes/list-likes.component.ts
+++ src/app/shared/list-likes/list-likes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ListLikesService } from './list-likes.service';
 import { listLikes } from 'src/app/classes/listLikes';
+import { LikesService } from '../likes/likes.service';
 
 @Component({
   selector: 'app-list-likes',
@@ -10,18 +11,34 @@ import { listLikes } from 'src/app/classes/listLikes';
 export class ListLikesComponent implements OnInit {
 
  @Input () resourse_id : number;
+
   likesList : listLikes[];
+  temp: any =[];
+  posts:any=[];
   constructor(private ser: ListLikesService) { }
 
-
-  ngOnInit() {
-    this.ser.getLikesList(this.resourse_id).subscribe(data => {
-      this.likesList = data,
-      console.log(this.likesList)
-    },
-     // (err: any) => console.log(err)
-     );
+ ngOnInit() {
+  this.ser.getLikesList(this.resourse_id).subscribe((posts:any)=>{
+    this.posts =posts.likes ;
+    this.temp = posts;
+    console.log(posts);
+    }
+    ,
+       (err: any) => console.log(err)
+    );
 
   }
+
+  reload(){
+    this.ser.getLikesList(this.resourse_id).subscribe((posts:any)=>{
+      this.posts =posts.likes ;
+      this.temp = posts;
+      console.log(posts);
+      }
+      ,
+         (err: any) => console.log(err)
+      );
+  }
+
 
 }
